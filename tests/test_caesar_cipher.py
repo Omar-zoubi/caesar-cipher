f6302db37561a797ecba2e9a6476b7748f07f324
Omar-zoubi <o.fandi02@gmail.com>
2021-06-04 19:54:05 +0300

    lab18

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,23 @@
-from caesar_cipher import __version__
+from caesar_cipher.caesar_cipher import encrypt,decrypt, crack
+def test_encrypt_one():
+    actual = encrypt('Omar zoubi',6)
+    print(actual)
+    expected = 'Usgx fuaho'
+    assert actual == expected
+    
+def test_encrypt_ones():
+    actual = encrypt('I live in Ramtha',13)
+    print(actual)
+    expected = 'V yvir va Enzgun'
+    assert actual == expected
 
+def test_decrypt_one():
+    actual = decrypt('V yvir va Enzgun',13)
+    expected = 'I live in Ramtha'
+    assert actual == expected
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_crack():
+    expected = 'cat is amazing, and awsume'
+    encrypted = encrypt(expected, 50)
+    actual = crack(encrypted)
+    assert actual == expected
